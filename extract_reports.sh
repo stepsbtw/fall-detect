9c15d60eb1d62eb3b8eeeaffe18060e6cbd6a877
RodrigoKasama <rodrigo.parracho@gmail.com>
2024-11-06 20:37:25 -0300

    Update extract_reports.sh Passa a executar apenas a combinação de cenário ausente no diretório de resultados

--- extract_reports.sh
+++ extract_reports.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 
-
 # Definindo as variáveis para cada loop
 cenarios=("Sc1_acc_T" "Sc1_gyr_T" "Sc1_acc_F" "Sc1_gyr_F" "Sc_2_acc_T" "Sc_2_gyr_T" "Sc_2_acc_F" "Sc_2_gyr_F" "Sc_3_T" "Sc_3_F" "Sc_4_T" "Sc_4_F")
 redenerural=("MLP" "CNN1D")
 sensores=("chest" "right" "left")
-contador=0
+
 # Loop aninhado
 for cenario in "${cenarios[@]}"; do
     for nn in "${redenerural[@]}"; do
         for sensor in "${sensores[@]}"; do
-		contador=$((contador + 1))
-		printf "%s %s %s\n" "$cenario" "$nn" "$sensor"
-		python3 training.py -s "$cenario" -p "$sensor" -nn "$nn"
+		if [ ! -e "./results/${cenario}_${nn}_${sensor}.json" ]; then
+
+			# echo "${cenario}_${nn}_${sensor}.json não existe"
+			python3 training.py -s "$cenario" -p "$sensor" -nn "$nn"
+		fi
         done
     done
 done
+
+
+# Agrupando resultados
+
