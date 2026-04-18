6eb2d8b723a9ee79ffab76046675d2d55fed7ed6
Caio Torkst <stepsbtw@gmail.com>
2026-04-18 14:47:23 -0300

    Revert "Merge branch 'main' of https://github.com/stepsbtw/fall-detect"
    This reverts commit 7d592e88e268cfc2126349cfd5d7965af40255a2, reversing
    changes made to c785c95882e0d15585df58a1c07af617c88aedf8.

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@ import os
 import random
 
 import numpy as np
-import torch
 
 ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), ".."))
 DATA_PATH = os.path.normpath(os.path.join(ROOT_DIR, "dataset"))
